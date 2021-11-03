27ca4e88a2fec012fa79a5dfea8c8ddb141de677
igoea20 <64656258+igoea20@users.noreply.github.com>
2021-11-03 15:21:43 +0100

    Added statistics
    Cleaned up statistics functions and added a basic graph

--- keylogParser.py
+++ keylogParser.py
@@ -1,13 +1,18 @@
 import statistics as stat
+import matplotlib.pyplot as plt
 
 bigrams = ['th', 'he', 'gh', 'nd', 'ne', 'in', 'er', 'an', 'ng', 'me', 'we', 'is'
 			'at', 'on', 'es', 'ay', 'or', 'hi']
-currentUser = 'aoife'
+currentUser = 'oskar'
 
 class KeylogsParser:
 	def __init__(self, bigrams, file_name):
 		self.bigrams = bigrams
 		self.file_name = file_name
+		self.present_bigrams = []
+		self.std_devs = []
+		self.means = []
+		self.variances = []
 		self.results = {}
 
 	def read_file(self):
@@ -29,16 +34,54 @@ class KeylogsParser:
 
 			print(self.results)
 
-	def get_sigma(self):
+	def get_stddev(self, bigram):
+		stddev = stat.stdev(self.results[bigram])
+		print('"' + bigram + '" standard deviation: ' + str(stddev))
+		return stddev
+
+	def get_mean(self, bigram):
+		mean = stat.mean(self.results[bigram])
+		print('"' + bigram + '" mean: ' + str(mean))
+		return mean
+
+	def get_variance(self, bigram):
+		var = stat.variance(self.results[bigram])
+		print('"' + bigram + '" variance: ' + str(var))
+		return var
+
+	#calculates the stats and prints them
+	def get_stats(self):
 		for bigram in self.bigrams:
 			if (bigram in self.results and len(self.results[bigram]) > 1):
-				print(bigram + ' ' + str(stat.stdev(self.results[bigram])))
+				self.std_devs.append(float(keylogsParser.get_stddev(bigram)))
+				self.means.append(float(keylogsParser.get_mean(bigram)))
+				self.variances.append(float(keylogsParser.get_variance(bigram)))
+				self.present_bigrams.append(bigram)
+		print(self.std_devs)
+		print(self.means)
+		print(self.variances)
 
+	def print_graph(self):
+		x = self.present_bigrams
+		y_std_dev = self.std_devs
+		y_mean = self.means
+		y_var = self.variances
 
+		#graph for std_Dev
+		plt.plot(x, y_std_dev, label = "Standard Deviation")
+		#graph for std_Dev
+		plt.plot(x, y_mean, label = "Mean")
+		#graph for std_Dev
+		plt.plot(x, y_var, label = "Variance")
+		plt.xlabel('bigrams')
 
+		plt.title('Statistics of bigrams')
+		plt.legend()
+		plt.show()
 
 
 if __name__ == "__main__":
 	keylogsParser = KeylogsParser(bigrams, f"keylogs/{currentUser}.txt")
 	keylogsParser.read_file()
-	keylogsParser.get_sigma()
+	keylogsParser.get_stats()
+	keylogsParser.print_graph()
